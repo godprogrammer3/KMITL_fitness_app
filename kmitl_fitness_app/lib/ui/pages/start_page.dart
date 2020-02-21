610ef111563a1f2802ddb7c0c8f1efc1a032629d
gofprogrammer <game47you@gmail.com>
2020-02-22 02:49:57 +0700

    Finished UI

--- kmitl_fitness_app/lib/ui/pages/start_page.dart
+++ kmitl_fitness_app/lib/ui/pages/start_page.dart
@@ -9,20 +9,60 @@ class StartPage extends StatelessWidget {
   }
 }
 
-
 class StartPageChild extends StatelessWidget {
   const StartPageChild({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
-        Container(
-          child: Center(
-            child: Text('KMITL FITNESS',style:TextStyle(color: Colors.black))
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/start_page_bg.png"),
+            fit: BoxFit.cover,
           ),
-         
         ),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height:MediaQuery.of(context).size.height * 0.4),
+              Text('KMITL',
+                  style: TextStyle(
+                      color: Color.fromARGB(255,255, 111, 0),
+                      fontFamily: 'Roboto',
+                      fontSize: 60)),
+              Text('FITNESS',
+                  style: TextStyle(
+                      color: Colors.white, fontFamily: 'Roboto', fontSize: 60)),
+              SizedBox(height: 80),
+              MaterialButton(
+                  height: MediaQuery.of(context).size.width * 0.118,
+                  minWidth: MediaQuery.of(context).size.width * 0.8,
+                  color: Color.fromARGB(255,255, 111, 0),
+                  textColor: Colors.white,
+                  child: Text('Login',style: TextStyle(
+                      color: Colors.white, fontFamily: 'Roboto', fontSize: 20,)),
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(34.0),
+                      side: BorderSide(color: Colors.transparent))),
+              SizedBox(height: 15),
+              MaterialButton(
+                  height: MediaQuery.of(context).size.width * 0.118,
+                  minWidth: MediaQuery.of(context).size.width * 0.8,
+                  color: Colors.white,
+                  textColor: Color.fromARGB(255,255, 111, 0),
+                  child: Text('Sign up',style: TextStyle(
+                      color: Color.fromARGB(255,255, 111, 0), fontFamily: 'Roboto', fontSize: 20,)),
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(34.0),
+                      side: BorderSide(color: Colors.transparent))),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
