6dd039486c9283685519c598cbcdd04e9bf68d20
Watcharakorn Thankaew <watcharakorn.yew@gmail.com>
2020-02-22 21:45:52 +0700

    Update pages.dart

--- kmitl_fitness_app/lib/ui/pages/pages.dart
+++ kmitl_fitness_app/lib/ui/pages/pages.dart
@@ -1,3 +1,10 @@
 export './login_page.dart';
 export './signup_page.dart'; 
 export './start_page.dart';
+
+export './navigation.dart';
+export './home_page.dart';
+export './class_page.dart';
+export './locker_page.dart';
+export './treadmill_page.dart';
+export './profile_page.dart';
