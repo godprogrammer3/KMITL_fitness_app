4e9786cfe7494fded0f5346130a5a8196bd2e6a2
Tanabodee Sangprajong <deempooh@gmail.com>
2020-03-14 19:48:28 +0700

    UI Done

--- kmitl_fitness_app/lib/ui/pages/locker_pagedetail.dart
+++ kmitl_fitness_app/lib/ui/pages/locker_pagedetail.dart
@@ -22,7 +22,7 @@ class _LockerPageDetailStateChild extends State<LockerPageDetailChild> {
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(
-            color: Colors.black,
+          color: Colors.orange[900],
         ),
         title: Text(
           'LockerDetail',
@@ -38,7 +38,46 @@ class _LockerPageDetailStateChild extends State<LockerPageDetailChild> {
         ],
       ),
       body: Center(
-        
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Your locker is UNLOCKED",
+              style: TextStyle(
+                  color: Colors.grey[600],
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20,),
+            Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                shape: BoxShape.circle,
+              ),
+              child: Icon(Icons.lock_open,size: 100,),
+            ),
+            SizedBox(height: 20,),
+            Container(
+              width: 200,
+              height: 50,
+                child: FlatButton(
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100),
+                        side: BorderSide(color: Colors.transparent)),
+                    color: Colors.grey[800],
+                    child: Text(
+                      "Close",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    )),
+            )
+          ],
+        ),
       ),
     );
   }
