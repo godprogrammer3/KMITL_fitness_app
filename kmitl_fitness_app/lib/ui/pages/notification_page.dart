fb435f80121de590af0cd54f01c6c03250f28d69
bobby <bobby.sithi@gmail.com>
2020-03-20 19:20:43 +0700

    notification page completed

--- kmitl_fitness_app/lib/ui/pages/notification_page.dart
+++ kmitl_fitness_app/lib/ui/pages/notification_page.dart
@@ -9,11 +9,53 @@ class NotificationPage extends StatelessWidget {
   }
 }
 
+var messages = const[
+  'My first message',
+  'My second message',
+  'My third message',
+  'My fourth message'
+
+];
+
 class NotificationPageChild extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: (
+              Text("Notification", style: TextStyle(color: Colors.orange[900]), textAlign: TextAlign.left,)
+            ),
+        backgroundColor: Colors.white,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {},
+            color: Colors.orange[900],
+          )
+        ],
+      ),
+      body: ListView.separated(
+        //itemCount: messages.length,
+        itemCount: 21,
+        separatorBuilder: (context,index)=> Divider(),
+        itemBuilder: (BuildContext context, int index) {
+          //var title = messages[index];
+        return ListTile(     
+          //trailing: Text('Z'),
+          //title : Text(title),
+          title : Text('Title $index'),
+          isThreeLine: true,
+          leading: CircleAvatar(
+            backgroundColor: Colors.grey,
+            child: Text('UI', style: TextStyle(color: Colors.white),),
+            
+          ),
+          subtitle: Text('Greyhound divisively hello coldly'),
+
+        );
+      },)
+    );
   }
 
 }
