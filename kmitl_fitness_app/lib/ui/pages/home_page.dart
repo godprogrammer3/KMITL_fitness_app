aba038b4b3b7852dcb72f5b2150185efdaf56467
Watcharakorn <watcharakorn.yew@gmail.com>
2020-04-13 00:55:02 +0700

    Update home page ui

--- kmitl_fitness_app/lib/ui/pages/home_page.dart
+++ kmitl_fitness_app/lib/ui/pages/home_page.dart
@@ -18,15 +18,12 @@ class HomePageChild extends StatefulWidget {
 }
 
 class _HomePageStateChild extends State<HomePageChild> {
-  final List<String> items = List<String>.generate(20, (i) => "News: ${++i}");
+  final List<String> items = List<String>.generate(3, (i) => 'i');
 
   @override
   void initState() {
     super.initState();
   }
-
-
-  
 
   @override
   Widget build(BuildContext context) {
@@ -52,38 +49,57 @@ class _HomePageStateChild extends State<HomePageChild> {
         ],
       ),
       body: ListView.builder(
+        padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 20.0),
         itemCount: items.length,
         itemBuilder: (contex, index) {
-          return SizedBox(
-            height: 320,
-            child: Card(
-              child: InkWell(
-                onTap: (){
-                  Navigator.of(context).push(MaterialPageRoute(
+          return Column(
+            children: <Widget>[
+              Card(
+                child: InkWell(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
                       builder: (context) => PostDetailPage(),
-                  ));
-                },
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 240.0,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage('assets/images/example.jpg'),
-                          fit: BoxFit.fitWidth,
+                    ));
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 200.0,
+                        //width: 300.0,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/post01.png'),
+                            fit: BoxFit.fill,
+                          ),
                         ),
                       ),
-                    ),
-                    ListTile(
-                      title: Text("${items[index]}",
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      subtitle: Text("${items[index]}"),
-                    ),
-                  ],
+                      ListTile(
+                        title: Text('3 STEPS เทคนิคฟิตหุ่นให้ลีน แบบนางงาม',
+                            style: TextStyle(
+                                height: 2,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Kanit')),
+                        subtitle: Text(
+                          'ก่อนอื่นต้องยินดีกับนักเรียนของเรา น้องฟ้าใส ที่ได้รางวัล Golden Tiara Ticket ในรายการ...',
+                          style: TextStyle(fontFamily: 'Kanit'),
+                        ),
+                      ),
+                      Divider(),
+                      Text('อ่านต่อ',
+                          style: TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Kanit',
+                              color: Colors.orange[900])),
+                      SizedBox(height: 10.0)
+                    ],
+                  ),
                 ),
               ),
-            ),
+              SizedBox(height: 10.0)
+            ],
           );
         },
       ),
