a51372e398ea12d1b434193ecfbade97d2ee8181
Watcharakorn <watcharakorn.yew@gmail.com>
2020-04-19 17:40:00 +0700

    Create admin more page ui

---
+++ kmitl_fitness_app/lib/ui/pages/admin_more_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:kmitl_fitness_app/data/entitys/entitys.dart';
+import 'package:kmitl_fitness_app/ui/pages/pages.dart';
+
+class AdminMorePage extends StatelessWidget {
+
+  List<Widget> _pageOption = [
+    AdminRewardPage(),
+    Container()
+  ];
+  
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('More'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: 'Reward',
+              ),
+              Tab(
+                text: 'Package',
+              ),
+            ],
+          )
+        ),
+        body: TabBarView(
+          children: _pageOption
+        ),
+      ),
+    );
+  }
+}
