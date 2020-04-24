964fdc368b2c87c487fac5eecc23be5543622722
Tanabodee Sangprajong <deempooh@gmail.com>
2020-04-25 01:16:41 +0700

    update

---
+++ kmitl_fitness_app/lib/ui/pages/admin_userlog_page.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:kmitl_fitness_app/data/entitys/user.dart';
+import 'package:kmitl_fitness_app/models/models.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+class Data2 {
+  final int year;
+  final int sales;
+
+  Data2(this.year, this.sales);
+}
+
+final data2 = [
+  new Data2(0, 1500000),
+  new Data2(1, 1735000),
+  new Data2(2, 1678000),
+  new Data2(3, 1890000),
+  new Data2(4, 1907000),
+  new Data2(5, 2300000),
+  new Data2(6, 2360000),
+  new Data2(7, 1980000),
+  new Data2(8, 2654000),
+  new Data2(9, 2789070),
+  new Data2(10, 3020000),
+  new Data2(11, 3245900),
+  new Data2(12, 4098500),
+  new Data2(13, 4500000),
+  new Data2(14, 4456500),
+  new Data2(15, 3900500),
+  new Data2(16, 5123400),
+  new Data2(17, 5589000),
+  new Data2(18, 5940000),
+  new Data2(19, 6367000),
+];
+
+_getSeriesData() {
+  List<charts.Series<Data2, int>> series = [
+    charts.Series(
+        id: "Sales",
+        data: data2,
+        domainFn: (Data2 series, _) => series.year,
+        measureFn: (Data2 series, _) => series.sales,
+        colorFn: (Data2 series, _) =>
+            charts.MaterialPalette.blue.shadeDefault)
+  ];
+  return series;
+}
+
+class AdminUserLogPage extends StatelessWidget {
+  final User user;
+  AdminUserLogPage({Key key, this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AdminUserLogPageChild(user: user);
+  }
+}
+
+class AdminUserLogPageChild extends StatefulWidget {
+  final User user;
+  const AdminUserLogPageChild({Key key, this.user}) : super(key: key);
+  @override
+  _AdminUserLogPageStateChild createState() =>
+      _AdminUserLogPageStateChild(user: user);
+}
+
+class _AdminUserLogPageStateChild extends State<AdminUserLogPageChild> {
+  final authenModel = AuthenModel();
+  final User user;
+
+  _AdminUserLogPageStateChild({this.user});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                SizedBox(height: 20,),
+                Center(
+                  child: Text(
+                    'จำนวนคนเข้าใช้',
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Container(
+                  height: 150.0,
+                  child: new charts.LineChart(
+                    _getSeriesData(),
+                    animate: true,
+                  ),
+                ),
+              ],
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
--- kmitl_fitness_app/lib/ui/pages/adminstatistic_page.dart
+++ kmitl_fitness_app/lib/ui/pages/adminstatistic_page.dart
@@ -1,138 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:kmitl_fitness_app/data/entitys/user.dart';
-import 'package:kmitl_fitness_app/models/models.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
-
-class SalesData {
-  final int year;
-  final int sales;
-
-  SalesData(this.year, this.sales);
-}
-
-final data = [
-  new SalesData(0, 1500000),
-  new SalesData(1, 1735000),
-  new SalesData(2, 1678000),
-  new SalesData(3, 1890000),
-  new SalesData(4, 1907000),
-  new SalesData(5, 2300000),
-  new SalesData(6, 2360000),
-  new SalesData(7, 1980000),
-  new SalesData(8, 2654000),
-  new SalesData(9, 2789070),
-  new SalesData(10, 3020000),
-  new SalesData(11, 3245900),
-  new SalesData(12, 4098500),
-  new SalesData(13, 4500000),
-  new SalesData(14, 4456500),
-  new SalesData(15, 3900500),
-  new SalesData(16, 5123400),
-  new SalesData(17, 5589000),
-  new SalesData(18, 5940000),
-  new SalesData(19, 6367000),
-];
-
-_getSeriesData() {
-  List<charts.Series<SalesData, int>> series = [
-    charts.Series(
-        id: "Sales",
-        data: data,
-        domainFn: (SalesData series, _) => series.year,
-        measureFn: (SalesData series, _) => series.sales,
-        colorFn: (SalesData series, _) =>
-            charts.MaterialPalette.blue.shadeDefault)
-  ];
-  return series;
-}
+import 'package:kmitl_fitness_app/data/entitys/entitys.dart';
+import 'package:kmitl_fitness_app/ui/pages/pages.dart';
 
 class AdminStatisticPage extends StatelessWidget {
   final User user;
+  final List<Widget> _pageOption = [
+    AdminIncomePage(),
+    AdminUserLogPage()
+  ];
+
   AdminStatisticPage({Key key, this.user}) : super(key: key);
-
+  
   @override
   Widget build(BuildContext context) {
-    return AdminStatisticPageChild(user: user);
-  }
-}
-
-class AdminStatisticPageChild extends StatefulWidget {
-  final User user;
-  const AdminStatisticPageChild({Key key, this.user}) : super(key: key);
-  @override
-  _AdminStatisticPageStateChild createState() =>
-      _AdminStatisticPageStateChild(user: user);
-}
-
-class _AdminStatisticPageStateChild extends State<AdminStatisticPageChild> {
-  final authenModel = AuthenModel();
-  final User user;
-
-  _AdminStatisticPageStateChild({this.user});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'Statistic',
-          style: TextStyle(color: Colors.white),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          //centerTitle: true,
+          //title: Text('More'),
+          title: TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: 'Income',
+              ),
+              Tab(
+                text: 'UserLog',
+              ),
+            ],
+          )
         ),
-        backgroundColor: Colors.orange[900],
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                SizedBox(height: 20,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    SizedBox(
-                      width: 30,
-                    ),
-                    Text(
-                      'รายได้',
-                      style: TextStyle(color: Colors.black, fontSize: 20),
-                    )
-                  ],
-                ),
-                SizedBox(height: 20,),
-                Container(
-                  height: 150.0,
-                  child: new charts.LineChart(
-                    _getSeriesData(),
-                    animate: true,
-                  ),
-                ),
-                SizedBox(height: 20,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    SizedBox(
-                      width: 30,
-                    ),
-                    Text(
-                      'จำนวนคนเข้าใช้',
-                      style: TextStyle(color: Colors.black, fontSize: 20),
-                    )
-                  ],
-                ),
-                SizedBox(height: 20,),
-                Container(
-                  height: 150.0,
-                  child: new charts.LineChart(
-                    _getSeriesData(),
-                    animate: true,
-                  ),
-                ),
-              ],
-            )
-          ],
-        )),
+        body: TabBarView(
+          children: _pageOption
+        ),
       ),
     );
   }
